6de1c9d785269a1e1dae820aa38e2c776043af6f
Xenovka <wendyanto18@gmail.com>
2021-11-14 12:51:42 +0700

    little tweak

---
+++ src/components/BaseHeader.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="header">
+    <h1 class="text-center">{{ username }}'s To do list</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["name"],
+  data() {
+    return {
+      username: this.name
+    };
+  }
+};
+</script>
+
+<style scoped>
+.header {
+  padding: 1rem 0;
+}
+
+.header > h1 {
+  font-size: 4.4rem;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,45 @@
 <template>
-  <div class="container">
-    <h1 class="text-center">To do list App</h1>
+  <div v-if="isUsernameExist" class="container">
+    <base-header :name="getUsername" />
   </div>
 </template>
 
 <script>
+import BaseHeader from "@/components/BaseHeader.vue";
+
 export default {
   name: "Home",
-  components: {}
+  components: {
+    BaseHeader
+  },
+  data() {
+    return {
+      username: "",
+      isUsernameExist: false
+    };
+  },
+  methods: {
+    insertUsername() {
+      this.username = prompt("Insert Your username:");
+
+      if (!this.username.length) {
+        alert("You have to insert your username!");
+        this.insertUsername();
+      } else {
+        this.isUsernameExist = true;
+        return;
+      }
+    }
+  },
+  computed: {
+    getUsername() {
+      return this.username;
+    }
+  },
+  mounted() {
+    this.insertUsername();
+  }
 };
 </script>
+
+<style scoped></style>
