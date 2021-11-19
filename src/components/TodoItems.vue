2d81fbca9ad2c676707946622897505273b85f95
Xenovka <wendyanto18@gmail.com>
2021-11-19 07:43:15 +0700

    change display and adding button (not finished)

--- src/components/TodoItems.vue
+++ src/components/TodoItems.vue
@@ -1,7 +1,10 @@
 <template>
-  <div class="container item-container d-flex justify-content-center">
-    <ul>
-      <li class="todo-item" v-for="item in items" :key="item">{{ item }}</li>
+  <div class="container item-container d-flex justify-content-center position-relative">
+    <ul class="item-wrapper row">
+      <div v-for="item in items" :key="item" class="d-flex">
+        <li class="todo-item col-lg-6 offset-lg-2">{{ item }}</li>
+        <button class="btn-delete col-lg-1">Delete</button>
+      </div>
     </ul>
   </div>
 </template>
@@ -35,7 +38,21 @@ export default {
   margin-top: 3rem;
 }
 
+.item-wrapper {
+  width: 100%;
+}
+
 .todo-item {
   font-size: 1.8rem;
+  font-weight: 500;
+  list-style: none;
+  padding: 1rem 1.4rem;
+  margin-bottom: 0.6rem;
+  background-color: #ccc;
+}
+
+.btn-delete {
+  padding: 1rem 1.4rem;
+  font-size: 1.4rem;
 }
 </style>
